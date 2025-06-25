dce44f4d8aaac868400b975d994b9a9091e37ef6
Aman Gupta <aman97703@gmail.com>
2025-06-26 00:31:18 +0530

    fix cokkies

--- src/server.js
+++ src/server.js
@@ -6,18 +6,28 @@ import { initDB } from "./config/db.js";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({
-  origin: "https://aboss-frontend.vercel.app", // Must match exactly
-  credentials: true, // Allows sending cookies from frontend
-}));
-
+app.use(
+  cors({
+    origin: "https://aboss-backend.onrender.com/", // or leave unset since it's same origin now
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/payment", paymentRoutes);
